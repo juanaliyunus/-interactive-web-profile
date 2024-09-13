2211363cc5fca33c92cde512a2072a67572264e2
enigma <aliyunusjuan@gmail.com>
2024-09-13 10:38:42 +0700

    add featur mobile navigation

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,6 +3,7 @@ import { Button } from "./ui/button";
 
 //components
 import Nav from "./Nav";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
@@ -22,7 +23,9 @@ const Header = () => {
             <Button>Hire me</Button>
           </Link>
         </div>
-        <div className="xl:hidden">mobile nav</div>
+        <div className="xl:hidden">
+          <MobileNav/>
+        </div>
       </div>
     </header>
   );
