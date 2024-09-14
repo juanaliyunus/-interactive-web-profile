799235e6cbbca09d4d295a34ad8f8487dfebbd03
enigma <aliyunusjuan@gmail.com>
2024-09-14 21:39:54 +0700

    add content

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -4,7 +4,7 @@ import CountUp from "react-countup";
 
 const stats = [
   {
-    num: 12,
+    num: 1,
     text: "Years of experience",
   },
   {
@@ -12,7 +12,7 @@ const stats = [
     text: "Projects completed",
   },
   {
-    num: 8,
+    num: 11,
     text: "Technologies mastered",
   },
   {
